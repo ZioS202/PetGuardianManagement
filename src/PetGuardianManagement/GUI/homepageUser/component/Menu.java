08b3b7688cfcb292f8b45bfdca9bcbf42a4d17ca
ZioS202 <thehao210502@gmail.com>
2024-04-21 10:36:04 +0700

    update GUI homepageUser

--- src/PetGuardianManagement/GUI/homepageUser/component/Menu.java
+++ src/PetGuardianManagement/GUI/homepageUser/component/Menu.java
@@ -4,6 +4,7 @@
  */
 package PetGuardianManagement.GUI.homepageUser.component;
 
+import PetGuardianManagement.GUI.homepageUser.event.EventMenuSelected;
 import PetGuardianManagement.GUI.homepageUser.model.Model_Menu;
 import java.awt.Color;
 import java.awt.GradientPaint;
@@ -21,9 +22,12 @@ import javax.swing.JFrame;
  */
 public class Menu extends javax.swing.JPanel {
 
-    /**
-     * Creates new form Menu
-     */
+    private EventMenuSelected event;
+
+    public void addEventMenuSelected(EventMenuSelected event) {
+        this.event = event;
+        listMenu1.addEventMenuSelected(event);
+    }
     public Menu() {
         initComponents();
         setOpaque(false);
@@ -109,7 +113,7 @@ public class Menu extends javax.swing.JPanel {
     protected void paintChildren(Graphics grphcs) {
         Graphics2D g2 = (Graphics2D) grphcs;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        GradientPaint g = new GradientPaint(0, 0, Color.decode("#1CB5E0"), 0, getHeight(), Color.decode("#000046"));
+        GradientPaint g = new GradientPaint(0, 0, Color.decode("#3a6186"), 0, getHeight(), Color.decode("#89253e"));
         g2.setPaint(g);
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
         g2.fillRect(getWidth() - 20, 0, getWidth(), getHeight());
---
+++ src/PetGuardianManagement/GUI/homepageUser/event/EventMenuSelected.java
@@ -0,0 +1,5 @@
+package PetGuardianManagement.GUI.homepageUser.event;
+
+public interface EventMenuSelected {
+    public void selected(int index);
+}
